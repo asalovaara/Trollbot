0594cf7a194061aec8c12857a3ebf7d1df3b8281
sumuh <muhli.susanna@gmail.com>
2021-05-26 17:11:33 +0300

    function for choosing reply

--- backend/controllers/trollbotRouter.js
+++ backend/controllers/trollbotRouter.js
@@ -1,12 +1,37 @@
 const trollbotRouter = require('express').Router()
 
-const botGreeting = { body: 'Hello, I am a bot.', user: 'Bot', date: '1.1.2021', id: 0 }
-
-let messages = [botGreeting]
-
 trollbotRouter.get('/', (req, res) => {
   res.json(messages)
 })
+
+const getGreeting = () => {
+  return { body: 'Hello, I am a bot.', user: 'Bot', date: '1.1.2021', id: 0 }
+}
+
+// Not working yet. Update this when classification of user messages is ready & when bot replies are ready
+// Move to separate file?
+const chooseReply = ( {message} ) => {
+  if (message.type == 'opening') {
+    // todo
+    return "Hi"
+  }
+  if (message.type == 'closing') {
+    // todo
+    return "Bye"
+  }
+  if (message.type == 'question') {
+    // todo
+    return "I don't know"
+  }
+  if (message.type == 'other') {
+    // todo
+    return "I don't understand"
+  }
+}
+
+const botGreeting = getGreeting()
+
+let messages = [botGreeting]
 
 // Adds given message and a answer from bot to the list and returns it
 trollbotRouter.post('/', (req, res) => {
@@ -19,6 +44,7 @@ trollbotRouter.post('/', (req, res) => {
     id: messages.length + 1
   }
   const replyObject = {
+    // body: chooseReply
     body: `You said "${body.message}".`,
     user: 'Bot',
     date: new Date().toISOString(),
@@ -34,5 +60,6 @@ trollbotRouter.delete('/', (req, res) => {
   messages = [botGreeting]
   res.json(messages)
 })
+
 
 module.exports = trollbotRouter
