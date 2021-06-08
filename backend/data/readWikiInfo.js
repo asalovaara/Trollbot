2329de4a6cbacd089fd2ac7d39edb09a359eb3a1
kafenoir <a.i.ojala@gmail.com>
2021-06-08 08:08:10 +0300

    alternative genre search from Wikidata

--- backend/data/readWikiInfo.js
+++ backend/data/readWikiInfo.js
@@ -1,9 +1,9 @@
 const axios = require('axios')
 
-let bandname = 'Porcupine Tree' // Example. Should work for most bands.
-let url = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=' + bandname + '&rvsection=0'
+let artist = 'The Beatles' // Example. Should work for most artists.
+let url = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=' + artist + '&rvsection=0'
 
-//Fetches the band's genre using Wikipedia API
+//Fetches artist's genre using WikiMedia Action API
 axios
     .get(url)
     .then(({ data }) => {
@@ -16,6 +16,73 @@ axios
             genres.indexOf('[') + 2, genres.indexOf(']')
         ).toLowerCase().split('|')[0]
 
-        console.log(genre)
+        console.log('WikiMedia Action API (only first genre):\n\tArtist:', artist, '\n\tGenre: ',genre,'\n')
 
     })
+
+//Alternative approach: Fetches artist's genre(s) from Wikidata
+
+// Dispatches SPARQL query to Wikidata Query Service, response in json format
+class SPARQLQueryDispatcher {
+    constructor(endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    query(sparqlQuery) {
+        const fullUrl = this.endpoint + '?query=' + encodeURIComponent(sparqlQuery);
+
+        return axios
+            .get(fullUrl, {
+                headers: {
+                    Accept: 'application/sparql-results+json'
+                }
+            })
+            .then((response) => {
+
+                let genres = response.data.results.bindings
+                return genres
+                
+            })
+    }
+}
+
+// Fetch Wikidata ID for a given artist (returns the ID of the first result so not very sophisticated atm)
+const getArtistID = async (artist) => {
+
+    url = 'https://www.wikidata.org/w/api.php?action=wbsearchentities&format=json&language=en&type=item&continue=0&search=' + artist
+
+    try {
+        const response = await axios.get(url)
+        return response.data.search[0].id
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+// Get Wikidata ID for a given artist, create a query dispatcher, query db for associated genres, list genres
+const getGenre = async (artist) => {
+
+    const artistID = await getArtistID(artist)
+    const endpointUrl = 'https://query.wikidata.org/sparql';
+    const sparqlQuery = `SELECT ?genre ?genreLabel WHERE {
+   wd:${artistID} wdt:P136 ?genre.
+   SERVICE wikibase:label {
+     bd:serviceParam wikibase:language "en" .
+   }
+}`;
+
+    const queryDispatcher = new SPARQLQueryDispatcher(endpointUrl);
+    const genres = await queryDispatcher.query(sparqlQuery)
+
+    console.log('Wikidata (all genres): \n\tArtist:', artist, '\n\tGenres:')
+    genres.forEach(gnr => console.log('\t\t', gnr.genreLabel.value))
+
+}
+
+getGenre(artist)
+
+
+
+
+
