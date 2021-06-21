45bb551b1cdb278c34bab8e2ea92ae1efd2c6fc4
macwille <vw.manninen@gmail.com>
2021-06-21 23:52:16 +0300

    Added middleware and logger

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 const app = require('./app')
 const http = require('http')
 
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
