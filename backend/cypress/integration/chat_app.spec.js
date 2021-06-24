e75fd760e3a770d09bc927cbac580a6d3a75468f
macwille <vw.manninen@gmail.com>
2021-06-24 05:18:57 +0300

    Cypress fix

--- backend/cypress/integration/chat_app.spec.js
+++ backend/cypress/integration/chat_app.spec.js
@@ -3,21 +3,12 @@ describe('Trollbot chat page end to end tests', function () {
   it('Front page can be visited', function () {
     cy.visit('http://localhost:3001')
     cy.contains('Hello')
-    // cy.contains('Chat window')
   })
 
   it('Can send a message', function () {
     cy.contains('Type message')
-    cy.get('#message').type('Test message')
+    cy.get('#message').type('Elvis')
     cy.get('#submit').click()
-    cy.contains('Test message')
+    cy.contains('Elvis')
   })
-
-  // it('Can clear messages', function () {
-  //   cy.contains('Send')
-  //   cy.get('#clear').click()
-  //   cy.contains('Test message').should('not.exist')
-  //   cy.contains('Hello, I am a bot.')
-  // })
-
 })
