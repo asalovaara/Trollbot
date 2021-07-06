d6aac611d25013a2738b2d3857e8aac784cccde6
samilait <sami.p.laitinen@gmail.com>
2021-07-06 22:07:03 +0300

    group chat frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,35 @@
 import React from 'react'
-import Chat from './components/Chat'
-import { useAppStyles } from './styles/AppStyles.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-const App = () => {
-  const classes = useAppStyles()
+import './index.css'
+import Home from './Home/Home'
+import ChatRoom from './ChatRoom/ChatRoom'
 
+function App() {
   return (
-    <div className={classes.container}>
-      <Chat />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/:roomId' component={ChatRoom} />
+      </Switch>
+    </Router>
   )
 }
 
 export default App
+
+// import React from 'react'
+// import Chat from './components/Chat'
+// import { useAppStyles } from './styles/AppStyles.js'
+
+// const App = () => {
+//   const classes = useAppStyles()
+
+//   return (
+//     <div className={classes.container}>
+//       <Chat />
+//     </div>
+//   )
+// }
+
+// export default App
