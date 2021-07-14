83d520ae9b16c21c855dd7ec26da8d3312e6ed7c
samilait <sami.p.laitinen@gmail.com>
2021-07-14 16:56:31 +0300

    App update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,39 +10,39 @@ import Login from './components/Login'
 import loginService from './services/login'
 import { useAppStyles } from './styles/AppStyles.js'
 
-const App = () => {
-  const classes = useAppStyles()
-  const [user, setUser] = useState(null)
+// const App = () => {
+//   const classes = useAppStyles()
+//   const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedUser')
-    if (loggedUserJSON) {
-      console.log('Found user in localstorage')
-      const tryLogin = async () => {
-        const loggedUser = JSON.parse(loggedUserJSON)
-        const userObject = await loginService.login({
-          username: loggedUser.username
-        })
-        setUser(userObject)
-      }
-      tryLogin()
-    }
-  }, [])
+//   useEffect(() => {
+//     const loggedUserJSON = window.localStorage.getItem('loggedUser')
+//     if (loggedUserJSON) {
+//       console.log('Found user in localstorage')
+//       const tryLogin = async () => {
+//         const loggedUser = JSON.parse(loggedUserJSON)
+//         const userObject = await loginService.login({
+//           username: loggedUser.username
+//         })
+//         setUser(userObject)
+//       }
+//       tryLogin()
+//     }
+//   }, [])
 
-  const conditionalRender = () => {
-    if (!user) {
-      return (
-        <>
-          <Login user={user} setUser={setUser} />
-        </>
-      )
-    }
-    return (
-      <>
-        <Chat user={user} setUser={setUser} />
-      </>
-    )
-  }
+//   const conditionalRender = () => {
+//     if (!user) {
+//       return (
+//         <>
+//           <Login user={user} setUser={setUser} />
+//         </>
+//       )
+//     }
+//     return (
+//       <>
+//         <Chat user={user} setUser={setUser} />
+//       </>
+//     )
+//   }
 
 function App() {
   return (
