0d550a610aa8ee3a51ee05b6a90e479d7e246058
kafenoir <a.i.ojala@gmail.com>
2021-07-21 11:35:00 +0300

    form based introduction (WIP)

--- backend/rasa/actions/actions.py
+++ backend/rasa/actions/actions.py
@@ -4,13 +4,15 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-from typing import Any, Text, Dict, List
+from typing import Dict, Text, List, Optional, Any
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormValidationAction
 from rasa_sdk.events import FollowupAction
 from rasa_sdk.events import UserUtteranceReverted
 from rasa_sdk.events import SlotSet
+from rasa_sdk.types import DomainDict
 import requests
 
 class ActionBotOpening(Action):
@@ -51,6 +53,43 @@ class ActionSetDecisionPhase(Action):
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         return [SlotSet("decision_phase", True)]
+
+class ValidateIntroductionForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_introduction_form"
+    
+    def validate_introductions_finished(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+
+        counter = tracker.get_slot('counter') + 1
+        number_of_users = tracker.get_slot('number_of_users')
+
+        if counter == number_of_users:
+            return {"introductions_finished": slot_value, "counter": 0.0}
+        else:
+            # validation failed, set this slot to None so that the
+            # user will be asked for the slot again
+            return {"introductions_finished": None, "counter": counter}
+
+class ActionIncrementCounter(Action):
+
+    def name(self) -> Text:
+
+        return "action_increment_counter"
+
+    def run(self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        counter = tracker.get_slot('counter') + 1
+
+        return [SlotSet("counter", counter)]
 
 class ActionSetGenreSlot(Action):
 
