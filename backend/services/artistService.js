dca847f092b699737a9826280b61f0db3659f0ee
sumuh <muhli.susanna@gmail.com>
2021-07-28 19:09:46 +0300

    Added text file of 1000 most popular artists, and a function for adding them to the database with artists of the same genre grouped together. (Genre is fetched from Wikipedia api)

--- backend/services/artistService.js
+++ backend/services/artistService.js
@@ -1,18 +1,21 @@
 const { MongoClient } = require('mongodb')
+const { getGenre } = require('./wikiService')
+const fs = require('fs')
+const readline = require('readline')
 
 const main = async () => {
   const mongoUrl = 'mongodb://localhost:27017'
   const client = new MongoClient(mongoUrl)
+  const filepath = '../data/artists.txt'
 
   try {
     await client.connect()
 
-    await addArtist(client, 'Ariana Grande', 'pop', 'she')
-    await addArtist(client, 'Abba', 'pop', 'they')
-    await addArtist(client, 'Muse', 'rock', 'they')
-    await addArtist(client, 'Lil Nas X', 'rap', 'he')
+    //await deleteAll(client)
+    //await addAllArtistsBasedOnGenre(client, filepath)
+    //await findArtistsByGenre(client, 'hip hop music')
+    await findAll(client)
 
-    await findArtists(client)
   } catch (e) {
     console.error(e)
   } finally {
@@ -21,27 +24,84 @@ const main = async () => {
 
 }
 
-// add an artist to the db if doesn't exist yet
-const addArtist = async (client, artistName, artistGenre, artistPronoun) => {
+// Go through all artists (lines) in file.
+// Add genre if doesn't exist yet.
+// Add artist to genre sub-document.
+const addAllArtistsBasedOnGenre = async (client, filepath) => {
+
+  let rl = readline.createInterface({
+      input: fs.createReadStream(filepath),
+      output: process.stdout,
+      console: false
+  })
+
+  for await (const line of rl) {
+    try {
+      const artistName = line
+      const genre = await getGenre(artistName)
+      await addGenre(client, genre)
+      await addArtist(client, genre, artistName)
+      console.log('Added ' + artistName + ' with genre ' + genre + '. Taking a break...')
+      // Added this sleep because I think the queries were executing too fast and they stopped after a few queries. This fixed it
+      await sleep(500)
+    } catch (e) {
+      console.error(e)
+    }
+
+  }
+}
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+// Add an artist to the collection if doesn't exist yet.
+// (Atm duplicates are added though so run deleteAll before adding.)
+const addArtist = async (client, genre, artistName) => {
   await client.db('rasalogs').collection('artists').updateOne(
-   { name: artistName, genre: artistGenre, pronoun: artistPronoun },
-   { $set: { name: artistName, genre: artistGenre, pronoun: artistPronoun } },
+    { genre: genre },
+    { $push: { name: artistName } },
+    { upsert: true }
+ )
+}
+
+// Add a genre to the collection if doesn't exist yet
+const addGenre = async (client, genre) => {
+  await client.db('rasalogs').collection('artists').updateOne(
+   { genre: genre },
+   { $set: { genre: genre } },
    { upsert: true }
  )
 }
 
-// delete artist from the db
+// Find all genres and their sub-document artists
+const findAll = async (client) => {
+  const result = await client.db('rasalogs').collection('artists').find( {} )
+  console.log('---------- database contents -----------')
+  await result.forEach( obj => {
+    console.log(obj)
+  })
+}
+
+// Find artists filtered by genre
+const findArtistsByGenre = async (client, genre) => {
+  const result = await client.db('rasalogs').collection('artists').find( {
+    genre: genre
+  } )
+  console.log('---------- artists of genre ' + genre + ' -----------')
+  await result.forEach( obj => {
+    console.log(obj)
+  })
+}
+
+// Delete artist from the collection
 const deleteArtist = async (client, artistName) => {
   await client.db('rasalogs').collection('artists').deleteOne({ name: artistName })
 }
 
-// query the db
-const findArtists = async (client) => {
-  const result = await client.db('rasalogs').collection('artists').find( {} )
-
-  await result.forEach( obj => {
-    console.log(obj)
-  })
+// Delete everything from the collection
+const deleteAll = async (client) => {
+  await client.db('rasalogs').collection('artists').deleteMany({})
 }
 
 main().catch(console.error)
