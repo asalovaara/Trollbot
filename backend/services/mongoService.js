7a84f70d719ff0ac06555670f08cfeeb6a652da1
kafenoir <a.i.ojala@gmail.com>
2021-07-29 08:49:00 +0300

    improved log readability, added type identifiers to event names, slot values added to slot events

--- backend/services/mongoService.js
+++ backend/services/mongoService.js
@@ -1,6 +1,10 @@
 const { MongoClient } = require('mongodb')
 const createWriter = require('csv-writer').createObjectCsvWriter
 
+const defaultActions = ['action_listen', 'action_restart', 'action_session_start', 'action_default_fallback', 'action_deactivate_loop', 'action_revert_fall', 'action_two_stage_fallback', 'action_default_ask_affirmation', 'action_default_ask_rephrase', 'action_back', 'action_unlikely_intent']
+
+const ignoredEvents = ['user_featurization']
+
 const main = async () => {
   const mongoUrl = 'mongodb://localhost:27017'
   const client = new MongoClient(mongoUrl)
@@ -23,13 +27,15 @@ const findEvents = async (client) => {
   await result.forEach( obj => {
     let arr = obj.events
     arr.forEach(obj => {
-      obj.timestamp = formatTimestamp(obj.timestamp)
-      if (obj.parse_data !== undefined) {
-        const intentName = obj.parse_data.intent.name
-        obj.intent = intentName
-      }
+
+      console.log(obj)
+
+      obj = formatEvent(obj)
+
     })
-    logMessage(arr)
+
+    const trimmedArr = removeIgnoredEvents(arr)
+    logMessage(trimmedArr)
   })
 }
 
@@ -69,6 +75,57 @@ const formatTimestamp = (epoch) => {
   var d = new Date(0)
   d.setUTCSeconds(utcSeconds)
   return d.toLocaleString()
+}
+
+const formatIntent = (obj) => {
+
+  if (obj.parse_data !== undefined) {
+    const intentName = obj.parse_data.intent.name
+    obj.intent = intentName
+  }
+
+  return obj
+}
+
+const formatEvent = (obj) => {
+
+  obj.timestamp = formatTimestamp(obj.timestamp)
+  
+  if (obj.event === 'action') {
+    if (defaultActions.includes(obj.name)) {
+      obj.event = 'triggered Rasa default action'
+      obj.name = 'action: ' + obj.name
+
+    } else if (obj.name.includes('utter')) {
+      obj.event = 'triggered bot response'
+      obj.name = 'response: ' + obj.name
+    } else {
+      obj.event = 'triggered custom action'
+      obj.name = 'action: ' + obj.name
+    }
+  } else if (obj.event === 'slot') {
+    obj.event = 'slot value was set'
+    obj.name = 'slot: ' + obj.name + ' | value: ' + obj.value
+  }
+    else if (obj.event === 'user') {
+    obj.event = 'user uttered'
+  } else if (obj.event === 'bot') {
+    obj.event = 'bot uttered'
+  } else if (obj.event === 'session_started') {
+    obj.event = 'started new conversation session'
+  }
+
+  obj = formatIntent(obj)
+
+  return obj
+}
+
+const removeIgnoredEvents = (arr) => {
+
+  const trimmedArr = arr.filter(obj => !(ignoredEvents.includes(obj.event)))
+
+  return trimmedArr
+
 }
 
 main().catch(console.error)
