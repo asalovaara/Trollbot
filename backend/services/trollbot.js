aac0e192f4f2fa9940b96c63e419a9f03dcbeda6
macwille <vw.manninen@gmail.com>
2021-07-30 10:19:18 +0300

    Removed legacy tests and functions

--- backend/services/trollbot.js
+++ backend/services/trollbot.js
@@ -1,5 +1,3 @@
-const getIntent = require('./intentService')
-const logger = require('../utils/logger')
 const { getRasaRESTResponse } = require('./rasaService')
 const { getGenreByName } = require('./spotifyService')
 
@@ -8,10 +6,6 @@ let replies = []
 
 const rasaAnswer = (data) => {
   return getRasaResponse(data)
-}
-
-const botAnswer = (data) => {
-  return getResponse(data.body)
 }
 
 const getRasaResponse = async (data) => {
@@ -44,92 +38,9 @@ const getRasaResponse = async (data) => {
 
 }
 
-const getResponse = async (userMessage) => {
-  logger.info('Entered trollbotAnswerController:getResponse().')
-  try {
-    const messageType = getMessageType(userMessage)
-
-    const reply = await chooseReply(userMessage, messageType)
-    logger.info(`User message: ${userMessage}`)
-    logger.info(`Bot reply: ${reply}`)
-
-    const messageObject = {
-      body: userMessage,
-      user: 'Human',
-      date: new Date().toISOString(),
-      id: messages.length + 1
-    }
-    const replyObject = {
-      body: reply,
-      user: 'Bot',
-      date: new Date().toISOString(),
-      id: messages.length + 2
-    }
-
-    messages = messages.concat([messageObject, replyObject])
-
-    return messages
-  } catch (e) {
-    console.error(e)
-  }
-}
-
-const getGreeting = () => {
-  return { body: 'Hello, I am a bot.', user: 'Bot', date: '1.1.2021', id: 0 }
-}
-
-const getMessages = () => {
-  return messages
-}
-
-const clearMessages = () => {
-  messages = [{ body: 'Hello, I am a bot.', user: 'Bot', date: '1.1.2021', id: 0 }]
-}
-
-const getMessageType = (userMessage) => {
-  try {
-    const intent = getIntent(userMessage)
-
-    if (intent === 'opening') {
-      return 'opening'
-    } else if (intent === 'closing') {
-      return 'closing'
-    } else if (intent == 'question') {
-      return 'question'
-    } else {
-      return 'other'
-    }
-  } catch (e) {
-    console.error(e)
-  }
-
-}
-
-const chooseReply = async (userMessage, messageType) => {
-  logger.info('Entered trollbotAnswerController:chooseReply()')
-
-  let repliesNumber = Math.floor(Math.random() * 3)
-
-  if (messageType == 'opening') {
-    return replies.opening[repliesNumber]
-  }
-  if (messageType == 'closing') {
-    return replies.closing[repliesNumber]
-  }
-  if (messageType == 'question') {
-    return replies.question[repliesNumber]
-  }
-  if (messageType == 'other') {
-    return await genreToRasa(userMessage)
-  }
-}
 
 const genreToRasa = async (artist) => {
 
-  //get genre from wikipedia
-  //const genre = await getGenre(artist)
-
-  //get genre from spotify
   const genre = await getGenreByName(artist)
 
   return genre
@@ -137,4 +48,4 @@ const genreToRasa = async (artist) => {
 }
 
 
-module.exports = { botAnswer, rasaAnswer, getGreeting, getMessages, getResponse, clearMessages, genreToRasa }
+module.exports = { rasaAnswer, genreToRasa }
