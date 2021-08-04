fd7cf257cf31303a20e9c59b2500a33a9a442070
samilait <sami.p.laitinen@gmail.com>
2021-08-04 09:59:20 +0300

    un-comment disable user login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-// import React, { useEffect, useState } from 'react'
+// import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import './index.css'
@@ -7,27 +7,27 @@ import Navigation from './components/groupchat/Navigation'
 import Home from './components/groupchat/RoomSelect'
 import ChatRoom from './components/groupchat/ChatRoom'
 import Login from './components/groupchat/Login'
-// import loginService from './services/login'
+import loginService from './services/login'
 import { Container, Box } from '@material-ui/core'
 
 const App = () => {
   const [user, setUser] = useState(null)
 
-  // useEffect(() => {
-  //   const loggedUserJSON = window.localStorage.getItem('loggedUser')
-  //   console.log('loggedUserJSON', loggedUserJSON)
-  //   if (loggedUserJSON) {
-  //     console.log('Found user in localstorage')
-  //     const tryLogin = async () => {
-  //       const loggedUser = JSON.parse(loggedUserJSON)
-  //       const userObject = await loginService.login({
-  //         name: loggedUser.name
-  //       })
-  //       setUser(userObject)
-  //     }
-  //     tryLogin()
-  //   }
-  // }, [])
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    console.log('loggedUserJSON', loggedUserJSON)
+    if (loggedUserJSON) {
+      console.log('Found user in localstorage')
+      const tryLogin = async () => {
+        const loggedUser = JSON.parse(loggedUserJSON)
+        const userObject = await loginService.login({
+          name: loggedUser.name
+        })
+        setUser(userObject)
+      }
+      tryLogin()
+    }
+  }, [])
 
   const conditionalRender = () => {
     if (!user) {
