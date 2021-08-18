d34cbe900b1c609a00300c0bac475cb3a37ee896
Lauri Halttunen <lauri.halttunen@helsinki.fi>
2021-08-18 22:55:25 +0300

    fixes and more stories

--- backend/rasa/actions/actions.py
+++ backend/rasa/actions/actions.py
@@ -5,7 +5,7 @@ from typing import Dict, Text, List, Optional, Any
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormValidationAction
-from rasa_sdk.events import FollowupAction, UserUtteranceReverted, SlotSet, ReminderScheduled, ReminderCancelled
+from rasa_sdk.events import FollowupAction, UserUtteranceReverted, SlotSet, ReminderScheduled, ReminderCancelled, Restarted
 from rasa_sdk.types import DomainDict
 import requests
 import datetime
