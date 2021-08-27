cca99133c1268b5166ec69747fd4e3e10f61c26f
vornsami <sami.vornanen@helsinki.fi>
2021-08-27 23:35:50 +0300

    docker-fixes

--- backend/rasa/rasa_start.sh
+++ backend/rasa/rasa_start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -m
 rasa run actions &
-rasa run --endpoints endpoints-docker.yml --credentials credentials-docker.yml --enable-api --cors "*"
+rasa run -p 5005 -m models_nice --endpoints endpoints-docker.yml --credentials credentials-docker.yml --enable-api --cors "*" &
+rasa run -p 5006 -m models_troll --endpoints endpoints-docker.yml --credentials credentials-docker.yml --enable-api --cors "*"
 fg %1
 
 
