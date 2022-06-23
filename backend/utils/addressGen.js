debecddbe71c80ae47037dfff10dff7eaf4cb5ff
vornsami <48880447+vornsami@users.noreply.github.com>
2022-06-23 13:28:41 +0300

    Update addressGen.js

--- backend/utils/addressGen.js
+++ backend/utils/addressGen.js
@@ -1,8 +1,8 @@
 const crypto = require('crypto')
 
 const generate = (length) => {
-  const letters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890"
-  var ret = ""
+  const letters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890'
+  var ret = ''
 
   for ( var i = 0; i < length; i++) {
     ret += letters.charAt(crypto.randomInt(42))
