0052a7ded2b9da958c0074d5529fa9d4b4df7aef
vornsami <sami.vornanen@helsinki.fi>
2022-06-26 18:17:11 +0300

    Fix to singlequotes

--- backend/tests/rasaService.test.js
+++ backend/tests/rasaService.test.js
@@ -4,8 +4,8 @@ const roomService = require('../services/roomService')
 describe('Rasa Service unit tests', () => {
 
   beforeAll(() => {
-    roomService.addRoom({name:"Jest", roomLink:"JestLink"})
-    roomService.addRoom({name:"Jest2", roomLink:"JestLink2"})
+    roomService.addRoom({name:'Jest', roomLink:'JestLink'})
+    roomService.addRoom({name:'Jest2', roomLink:'JestLink2'})
     saveBotMessage({ recipient_id: 'Jest', text: 'Test' })
     saveBotMessage({ recipient_id: 'Jest2', text: 'Test2' })
   })
