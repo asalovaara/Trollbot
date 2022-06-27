bd56a1fb1278b4623e0a9150db55cfb35f4e2bd1
vornsami <48880447+vornsami@users.noreply.github.com>
2022-06-27 16:21:45 +0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const TITLE = process.env.REACT_APP_WEBSITE_NAME || 'Website Name'
+export const TITLE = process.env.REACT_APP_WEBSITE_NAME || 'Website Placeholder Name'
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api'
 export const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL || ''
 export const SOCKET_ENDPOINT = process.env.REACT_APP_SOCKET_ENDPOINT || 'localhost:3001'
